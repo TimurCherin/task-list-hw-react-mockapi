7f49243bd15ad8db01fb50b5bb0ebe8875f7ebf8
TimurCherin <109847059+TimurCherin@users.noreply.github.com>
2025-04-12 16:28:57 +0300

    selectors-react

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,34 +1,31 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchContacts, deleteContact } from '../../redux/contactsSlice';
+import { selectFilteredContacts } from '../../redux/selectors';
 import styles from './ContactList.module.css';
 
 const ContactList = () => {
-    const { items, isLoading, error } = useSelector((state) => state.contacts);
-    const filter = useSelector((state) => state.filter);
-    const dispatch = useDispatch();
+  const filteredContacts = useSelector(selectFilteredContacts);
+  const { isLoading, error } = useSelector((state) => state.contacts);
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch(fetchContacts());
-    }, [dispatch]);
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
 
-    const filteredContacts = items.filter(contact =>
-        contact.name.toLowerCase().includes(filter.toLowerCase())
-    );
+  if (isLoading) return <p>Loading contacts...</p>;
+  if (error) return <p>Error: {error}</p>;
 
-    if (isLoading) return <p>Loading contacts...</p>;
-    if (error) return <p>Error: {error}</p>;
-
-    return (
-        <ul className={styles.list}>
-            {filteredContacts.map(({ id, name, number }) => (
-                <li key={id} className={styles.item}>
-                    {name}: {number}
-                    <button onClick={() => dispatch(deleteContact(id))}>Delete</button>
-                </li>
-            ))}
-        </ul>
-    );
+  return (
+    <ul className={styles.list}>
+      {filteredContacts.map(({ id, name, number }) => (
+        <li key={id} className={styles.item}>
+          {name}: {number}
+          <button onClick={() => dispatch(deleteContact(id))}>Delete</button>
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default ContactList;
